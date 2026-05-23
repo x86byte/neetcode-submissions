e49a406ce28adb1b17f3e2ba454c287a558d13bb
x86byte <111459558+x86byte@users.noreply.github.com>
2026-05-23 12:35:22 +0100

    Add: count-squares - submission-2

---
+++ Data Structures & Algorithms/count-squares/submission-2.cpp
@@ -0,0 +1,82 @@
+class CountSquares {
+    vector<vector<int>> points;
+    size_t hc;
+    vector<int> m;
+public:
+    CountSquares()
+    {
+    }
+    
+    void add(vector<int> point)
+    {
+        m.push_back(point[0]);
+        for(auto i : point)
+            cout << i;
+        cout << endl;
+        points.push_back(point);
+    }
+    int to_s(int r, int c)
+    {
+        for(auto rr : points)
+            for(int cc = 0; cc < 1; cc++){
+                if(rr[cc] == r && rr[cc + 1] == c)
+                    return 1;
+            }
+        return 0;
+    }
+    int count(vector<int> point)
+    {
+        size_t r = point[0];
+        size_t c = point[1];
+        int max = *max_element(m.begin(), m.end());
+        if(to_s(r, c))
+            return -2;
+
+        add(point);
+        size_t rs = points.size();
+        size_t cs = points[0].size();
+        size_t ctr =0;
+        int into =0;
+        for(int rr = 0; rr < rs; rr++)
+        {
+            for(int cc = 0; cc <= cs; cc++)
+            {
+                if(r <  0|| c < 0)
+                    return 0;
+                size_t rb = r;
+                while(r >= max)
+                {
+                    cout << "testing1 : " << r << " " << c << endl;
+                    if(r <  0)
+                        break;
+                    if(to_s(r-1,c)){
+                        ctr++;
+                        break;
+                    }
+                    r--;
+                }
+                r = rb;
+                while(r <= max)
+                {
+                    cout << "testing2 : " << r << " " << c << endl;
+                    if(to_s(r,c)){
+                        ctr++;
+                        r = rb;
+                        break;
+                    }
+                    if(ctr == 2)
+                        return 1;
+                    if(r > rb)
+                        break;
+                    r++;
+                }
+                r = rb;
+                if(ctr == 2)
+                    return 1;
+            }
+        }
+        return 0;
+    };
+};
+
+
