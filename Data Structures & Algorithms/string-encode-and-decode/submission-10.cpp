ac1cc6b46cfaeb443fd4b9a8b93133a58c35d690
x86byte <111459558+x86byte@users.noreply.github.com>
2026-05-25 15:52:23 +0100

    Add: string-encode-and-decode - submission-10

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-10.cpp
@@ -0,0 +1,42 @@
+// writing another solution instead of AES solution
+
+class Solution {
+public:
+
+    string encode(vector<string>& s)
+    {
+        if(s.size() == 1)
+        {
+            return s[0];
+        }
+        string re;
+        for(auto ss : s){
+            re+= ss;
+            re+='#';
+        }
+        return re;
+    }
+
+    vector<string> decode(string s)
+    {
+        if(!s.size())
+            return {""};
+        if(s.size() == 1)
+            return {s};
+        vector<string> re;
+        string sv;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == '#' || i == s.size() - 1)
+            {
+                if(s[i] != '#')
+                    sv+=s[i];
+                re.push_back(sv);
+                sv.clear();
+                continue;
+            }
+            sv+=s[i];
+        }
+        return re;
+    }
+};
