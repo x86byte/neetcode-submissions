bd1d30e91342eb61f2631a829a168e21e548e418
x86byte <111459558+x86byte@users.noreply.github.com>
2026-05-25 17:26:33 +0100

    Add: string-encode-and-decode - submission-27

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-27.cpp
@@ -0,0 +1,72 @@
+// writing another solution instead of AES solution
+
+class Solution {
+    char del = 227;
+    int emp = 0;
+public:
+
+    string encode(vector<string>& s)
+    {
+        const size_t sz = s.size();
+        if(!sz)
+        {
+            emp = 1;
+            return "";
+        }
+        if(sz == 1)
+        {
+            if(!s[0].size())
+                return "";
+        }
+        string enc;
+        for(int i = 0; i < sz; i++)
+        {
+            if(!s[i].size()){
+                enc+= 128;
+                continue;
+            }
+            enc += s[i];
+            if(i < sz - 1)
+            {
+                enc += del;
+            }
+        }
+        const size_t ez = enc.size();
+        for(int i = 0; i < ez; i++)
+        {
+            enc[i] = enc[i] + (i % 10);
+        }
+        return enc;
+    }
+
+    vector<string> decode(string s)
+    {
+        const size_t sz = s.size();
+        if(emp)
+            return {};
+        vector<string> re;
+        string rs;
+        if(sz == 1 && s[0] != -128)
+            return {s};
+        if(!sz)
+            return {""};
+        for(int i = 0; i < sz; i++)
+        {
+            s[i] = s[i] - (i % 10);
+            if(s[i] == -128)
+            {
+                re.push_back("");
+                continue;
+            }
+            if(s[i] == del || i == sz - 1){
+                if(i == sz - 1)
+                    rs += s[i];
+                re.push_back(rs);
+                rs.clear();
+                continue;
+            }
+            rs+=s[i];
+        }
+        return re;
+    }
+};
