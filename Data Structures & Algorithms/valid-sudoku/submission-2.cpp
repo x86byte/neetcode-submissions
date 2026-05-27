4a339e2afca85b4689d4bb7d4c9972b924502efa
x86byte <111459558+x86byte@users.noreply.github.com>
2026-05-27 18:40:16 +0100

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> b;
+        for(int r = 0; r < board.size(); r++)
+            for(int c = 0; c < board[0].size(); c++)
+            {
+                const short e=  board[r][c];
+                if((char)(e) == '.')
+                    continue;
+                string rr = string(1, e) + " row " + string(1, r);
+                string cc = string(1, e) + " col " + string(1, c);
+                string bx = string(1, e) + " box " + string(1, r / 3) + "/" + string(1, c / 3);
+                if(!b.insert(rr).second || !b.insert(cc).second || !b.insert(bx).second)
+                    return false;
+            }
+        return true;
+    }
+};
