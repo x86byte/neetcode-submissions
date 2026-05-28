38b4e27cec4de77673c5f6d9d3e275e82a2e993e
x86byte <111459558+x86byte@users.noreply.github.com>
2026-05-28 14:16:41 +0100

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& n) {
+        if(!n.size())
+            return 1;
+        set<int> s;
+        sort(n.begin(), n.end());
+        for(auto i : n)
+            s.insert(i);
+        size_t ctr =0;
+        vector<int> v;
+        for(auto i : s)
+            v.push_back(i);
+        for(int i = 0; i < v.size(); i++)
+        {
+            if(i+1 < v.size())
+                if(v[i]+1 == v[i+1])
+                    ctr++;
+        }
+
+        return ctr +1;
+    }
+};
